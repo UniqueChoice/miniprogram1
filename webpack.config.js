4114baa4ca360b4b39e54ae437398fc061af8650
wjg <279076265@163.com>
2021-11-06 11:43:10 +0800

    修改配置文件，自动转义wxs插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,92 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const babel = require('@babel/core')
-const less = require('less')
+// const less = require('less')
 
 module.exports = {
-  mode: 'development',
-  watch: true,
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'dist')
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-		patterns:[
-		  {
-			from: '**/*.wxml',
-			to: './',
-			context: './src'
-		  },
-		  {
-			from: '**/*.json',
-			to: './',
-			context: './src'
-		  },
-		  {
-			from: '**/*.jpg',
-			to: './',
-			context: './src'
-		  },
-		  {
-			from: '**/*.png',
-			to: './',
-			context: './src'
-		  },
-		  {
-			from: '**/*.wxss',
-			to: './',
-			context: './src'
-		  },
-		  {
-			from: '**/*.css',
-			to: './',
-			context: './src'
-		  },
-		  {
-			from: '**/*.less',
-			to: './',
-			context: './src',
-			transform:{
-				transformer(content, path) {
-					return less.render(content.toString()).then(function (output) {
-					  return output.css;
-					});
-				}				
-			},
-			transformPath: function(target) {
-               return target.replace('.less', '.wxss');
-            }
-		  },
-		  {
-			from: '**/*.js',
-			globOptions: {
-				dot: true,
-				gitignore: true,
-				ignore: ["**/*.test.js", "**/*.spec.js"],
-			  },
-			to: './',
-			context: './src'//,
-			// transform:{
-				// transformer(content, path) {
-					// const newCode = babel.transformSync(content, {
-						// babelrc: true,
-						// "presets": ["@babel/env"]
-					// }).code;
-				  // return Promise.resolve(newCode.toString());
-				// },
-			// }
-		  }
-		]
-	})
-  ]
+    mode: 'development',
+    watch: true,
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: '**/*.wxml',
+                    to: './',
+                    context: './src'
+                },
+                {
+                    from: '**/*.json',
+                    to: './',
+                    context: './src'
+                },
+                // {
+                // from: '**/*.jpg',
+                // to: './',
+                // context: './src'
+                // },
+                {
+                    from: '**/*.png',
+                    to: './',
+                    context: './src'
+                },
+                {
+                    from: '**/*.wxss',
+                    to: './',
+                    context: './src'
+                },
+                {
+                    from: '**/*.wxs',
+                    to: './',
+                    context: './src',
+                    transform: {
+                        transformer(content, path) {
+                            const newCode = babel.transformSync(content, {
+                                babelrc: true,
+                                "presets": ["@babel/env"]
+                            }).code;
+                            return Promise.resolve(newCode.toString());
+                        },
+                    }
+                },
+                {
+                    from: '**/*.js',
+                    globOptions: {
+                        dot: true,
+                        gitignore: true,
+                        ignore: ["**/*.test.js", "**/*.spec.js"],
+                    },
+                    to: './',
+                    context: './src'//,
+                    // transform: {
+                    //     transformer(content, path) {
+                    //         const newCode = babel.transformSync(content, {
+                    //             babelrc: true,
+                    //             "presets": ["@babel/env"]
+                    //         }).code;
+                    //         return Promise.resolve(newCode.toString());
+                    //     },
+                    // }
+                }
+                // {
+                //     from: '**/*.less',
+                //     to: './',
+                //     context: './src',
+                //     transform: {
+                //         transformer(content, path) {
+                //             return less.render(content.toString()).then(function (output) {
+                //                 return output.css;
+                //             });
+                //         }
+                //     },
+                //     transformPath: function (target) {
+                //         return target.replace('.less', '.wxss');
+                //     }
+                // }
+            ]
+        })
+    ]
 }
